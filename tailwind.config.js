b73429b066808e81d90cda645f0f24ea2969e654
Jayanti Mahato <142143785+NITianJayanti@users.noreply.github.com>
2025-04-19 23:02:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js (ESM style)
+import daisyui from "daisyui";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+};
